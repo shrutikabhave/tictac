30c3f48abe0bb72d9a9a2b975cb112cb8788831f
shrutikabhave <45714186+shrutikabhave@users.noreply.github.com>
2019-01-31 16:34:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@
 const grid = [];
 const GRID_LENGTH = 3;
 let turn = 'X';
+var win = false;
 
 function initializeGrid() {
     for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
@@ -68,15 +69,31 @@ function renderMainGrid() {
     const parent = document.getElementById("grid");
     const columnDivs = getColumns();
     parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+    if ( checkWinner() ) {
+        win = true;
+        setMessage(turn + " WON!!");
+    }
 }
 
 function onBoxClick() {
     var rowIdx = this.getAttribute("rowIdx");
     var colIdx = this.getAttribute("colIdx");
-    let newValue = 1;
-    grid[colIdx][rowIdx] = newValue;
-    renderMainGrid();
-    addClickHandlers();
+    
+    if( grid[colIdx][rowIdx] == 0 && !win){
+        if( turn == 'X'){
+            let newValue = 1;
+            grid[colIdx][rowIdx] = newValue;     
+            renderMainGrid();
+            addClickHandlers();
+            turn = '0';
+        }
+        if( !win ){
+            computersTurn();
+        }
+    } else if( win ){
+        setMessage("Game Over!");
+    }
+    
 }
 
 function addClickHandlers() {
@@ -84,6 +101,73 @@ function addClickHandlers() {
     for (var idx = 0; idx < boxes.length; idx++) {
         boxes[idx].addEventListener('click', onBoxClick, false);
     }
+}
+
+function computersTurn(){
+    let checked = false;
+
+    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
+        for (let rowIdx = 0; rowIdx < GRID_LENGTH;rowIdx++) {
+            if( grid[colIdx][rowIdx] == 0 ){
+                grid[colIdx][rowIdx] = 2;
+                checked = true;
+                break;
+            }
+        }
+
+        if( checked ){
+            break;
+        }
+    }
+    
+    renderMainGrid();
+    addClickHandlers();
+    
+    turn = 'X';
+}
+
+function setMessage( msg){
+    document.getElementById("message").innerHTML = msg;
+}
+
+function checkWinner( ){
+    //row win
+    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {    
+        if( ( grid[colIdx][0] != 0 && grid[colIdx][1] != 0 && grid[colIdx][2] != 0 && 
+            grid[colIdx][0] == grid[colIdx][1] && grid[colIdx][2] == grid[colIdx][0] ) ){
+            return true;
+        }
+    }
+    //col win
+    for (let rowIdx = 0;rowIdx < GRID_LENGTH; rowIdx++) {    
+        if( ( grid[0][rowIdx] != 0 && grid[1][rowIdx] != 0 && grid[2][rowIdx] != 0 && 
+            grid[0][rowIdx] == grid[1][rowIdx] && grid[0][rowIdx] == grid[2][rowIdx] ) ){
+            return true;
+        }
+    }
+    //diagonal win
+    if( ( grid[0][0] != 0 && grid[1][1] != 0 && grid[2][2] != 0 && 
+            grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] ) ){
+        return true;
+    } else if( ( grid[0][2] != 0 && grid[1][1] != 0 && grid[2][0] != 0 && 
+            grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0] ) ){
+        return true;
+    }
+    return false;
+}
+
+function startGame() {
+    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
+        const tempArray = [];
+        for (let rowidx = 0; rowidx < GRID_LENGTH;rowidx++) {
+            grid[colIdx][rowidx]= 0;
+        }
+    } 
+    win = false;
+    turn = 'X';
+    setMessage('');
+    renderMainGrid();
+    addClickHandlers();
 }
 
 initializeGrid();
